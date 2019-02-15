d47af3f2735096b4f6c969d4050b7d9e967a0beb
MigueValiente <miguevaliente96@gmail.com>
2019-02-15 17:57:41 +0100

    ejercicio process con modal

--- bootstrapV4/js/progress.js
+++ bootstrapV4/js/progress.js
@@ -5,3 +5,21 @@ function instalar(){
         },i*100)
     }
 }
+
+function instalar2(){
+    for(let i= 0;i<= 100;i++){
+        setTimeout(function(){
+            $("#barra2").css("width",i+"%");
+        },i*100)
+    }
+}
+
+// function instalar2(){
+//     let ancho = 10;
+//     while(ancho<=100){
+//         setTimeout(function(){
+//             $("#barra2").css("width",(ancho)+"%");
+//             ancho+= Math.random()*100;
+//         },Math.random()*3000+100)
+//     }
+// }
